3dcb4d3ae2f09d2620288a53305c8fd3b40868f8
Lintang Gilang Pratama <67200488+lintanggilang@users.noreply.github.com>
2023-09-09 05:50:55 +0700

    Update app.py

--- app.py
+++ app.py
@@ -10,9 +10,12 @@ if uploaded_file is not None:
     st.image(uploaded_file, caption='Uploaded Image.', use_column_width=True)
     st.write("")
     st.write("Recognizing...")
+    
+    # Convert the uploaded BytesIO stream to bytes
+    uploaded_file_bytes = uploaded_file.getvalue()
 
     reader = easyocr.Reader(['en'], gpu=False)
-    result = reader.readtext(uploaded_file, detail=0, paragraph=True)
+    result = reader.readtext(uploaded_file_bytes, detail=0, paragraph=True)
 
     df = pd.DataFrame()
     df['result'] = result
