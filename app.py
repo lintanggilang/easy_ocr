e617eb9f036a0e2058d29f6eaa4f3025be990a9d
Lintang Gilang Pratama <67200488+lintanggilang@users.noreply.github.com>
2023-09-09 05:54:13 +0700

    Update app.py

--- app.py
+++ app.py
@@ -7,17 +7,23 @@ st.title('OCR App with EasyOCR')
 uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
 
 if uploaded_file is not None:
-    st.image(uploaded_file, caption='Uploaded Image.', use_column_width=True)
-    st.write("")
-    st.write("Recognizing...")
+    col1, col2 = st.beta_columns(2)
     
-    # Convert the uploaded BytesIO stream to bytes
-    uploaded_file_bytes = uploaded_file.getvalue()
+    # Display the uploaded image in the left column
+    with col1:
+        st.image(uploaded_file, caption='Uploaded Image.', use_column_width=True)
+    
+    # OCR process and display the result in the right column
+    with col2:
+        st.write("Recognizing...")
+        
+        # Convert the uploaded BytesIO stream to bytes
+        uploaded_file_bytes = uploaded_file.getvalue()
 
-    reader = easyocr.Reader(['en'], gpu=False)
-    result = reader.readtext(uploaded_file_bytes, detail=0, paragraph=True)
+        reader = easyocr.Reader(['en'], gpu=False)
+        result = reader.readtext(uploaded_file_bytes, detail=0, paragraph=True)
 
-    df = pd.DataFrame()
-    df['result'] = result
+        df = pd.DataFrame()
+        df['result'] = result
 
-    st.dataframe(df)
+        st.table(df)
